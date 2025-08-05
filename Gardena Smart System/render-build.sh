6699294983d88b9bb76295143ff4867fa4469abf
iMietkowy <wielgosz.kamill@gmail.com>
2025-08-05 18:35:04 +0200

    fix issue with build script 7

--- Gardena Smart System/render-build.sh
+++ Gardena Smart System/render-build.sh
@@ -5,13 +5,14 @@ set -o errexit
 # Krok 1: Instalacja zależności dla serwera
 echo "==> Installing server dependencies..."
 cd server
-npm install
+npm ci
+
 cd .. # Wracamy do głównego folderu
 
 # Krok 2: Instalacja zależności dla klienta
 echo "==> Installing client dependencies..."
 cd client
-npm install
+npm ci # Używamy npm ci, aby zainstalować WSZYSTKIE zależności, w tym vite
 
 # Krok 3: Budowanie klienta (jesteśmy już w folderze client)
 echo "==> Building client..."
